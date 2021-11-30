f1b93c4f33ebf9b0c1322bdd02696e636a2bc558
oderao <34449885+oderao@users.noreply.github.com>
2021-11-30 10:13:14 +0000

    update indent

--- flegeapp/utils/__init__.py
+++ flegeapp/utils/__init__.py
@@ -472,7 +472,7 @@ def get_size(item):
     size = frappe.db.get_value('Item',{'name':item},'item_size')
     image = frappe.db.get_value('Item',{'name':item},'image')
   
-        return {'size':size,'image':image}
+    return {'size':size,'image':image}
 
 @frappe.whitelist()
 def get_carebox_items(carebox):
